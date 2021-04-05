4d8f6beb4c60c441d7796a0930a631fde53a8b04
LakshmiBestha <lakshmib633@gmail.com>
2021-04-05 17:12:04 +0530

    updated employee details

--- src/com/practise/Employee.java
+++ src/com/practise/Employee.java
@@ -21,7 +21,7 @@ public static void main(String args[])
 {
 Address address1= new Address("atp","AP","INDIA");
  Employee emp=new Employee(1,"lucky",address1);
- 
+ Employee emp1=new Employee(2,"jaffa",address1);
  emp.display();
 }
 }
